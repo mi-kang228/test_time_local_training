ff7d9a40c870f065db6a5dc40a4fb2a491a8f6b5
mi-kang228 <mikang19960228@gmail.com>
2025-12-01 15:28:19 +0900

    Update run.py

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ run.py : Entry point — full pipeline execution
 """
 
 from model import MLP
-from utils import load_csv, split_data, train_model
+from utils import load_csv, split_and_scale, train_model
 from ttlt import ttlt_prediction
 import torch
 
